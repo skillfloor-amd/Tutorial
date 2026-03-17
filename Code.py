8c6e6ea943a11f4bf60f43fe9a2d1e191ad35668
skillfloor-amd <anukul.diwane@skillfloor.com>
2026-03-17 18:18:46 +0530

    Added average function in Code.py

--- Code.py
+++ Code.py
@@ -6,3 +6,17 @@ def table():
         print(f"{n} * {i} = {n * i}")
 
 table()
+
+
+# Write a python function to calculate average 3 User inputs
+
+def average():
+    num1 = float(input("Enter first number : "))
+    num2 = float(input("Enter second number : "))
+    num3 = float(input("Enter third number : "))
+
+    total = num1 + num2 + num3
+    avg = total / 3
+    print(avg)
+    
+average()
